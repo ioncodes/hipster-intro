d1bfb993a349d1961e8817852fa3d867763f6418
ion <ionix@protonmail.com>
2017-08-08 23:09:32 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod rasterbar;
+
+use rasterbar::Rasterbar;
+
+fn main() {
+    let rasterbar = Rasterbar::new();
+}
---
+++ src/rasterbar.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct Rasterbar {
+    pub height: u32,
+    pub length: u32,
+    pub colors: Vec<u32>,
+}
+
+impl Rasterbar {
+    pub fn new() -> Rasterbar {
+        unimplemented!();
+    }
+}
